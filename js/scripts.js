b0073b6b10bf86e4ef75ef996c7b6272ca9922ad
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-09-13 01:05:06 +0200

    EXERCISE 1. With Vanilla JS get values of inputs for sum it
    - Created structure of the form and result
    - Added 'text', 'button', 'link', 'form' and 'check' scss components
    - Created function for sum numbers and print the result
    - Added description for this example with Vanilla JS

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@
 // =================================================
 
 import * as footer from './modules/layouts/_layouts-footer.js'
+import * as exercise1 from './modules/pages/_pages-exercise-1.js'
 
 
 
 document.addEventListener('DOMContentLoaded', function () {
 	footer.init();
+	exercise1.init();
 }, false);
