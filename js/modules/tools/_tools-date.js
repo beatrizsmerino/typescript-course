bde349c3ffe93d9200a636626388e5346c70cae7
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-09-12 22:44:34 +0200

    LAYOUT. Created basic layout with 'header', 'main' and 'footer'
    - Created structure of project with partials scss and modules js
    - Added partials scss: 'abstracts', 'base', 'components', 'layouts' and 'pages' folders
    - Added modules js: 'tools', 'components' and 'layouts' folders
    - Added header and footer sticky
    - Added dynamic current year to the copyright

---
+++ js/modules/tools/_tools-date.js
@@ -0,0 +1,18 @@
+// TOOLS
+// date
+// =================================================
+
+
+
+
+
+function getCurrentYear() {
+	const date = new Date();
+	const currentYear = date.getFullYear();
+
+	return currentYear;
+}
+
+
+
+export { getCurrentYear }
