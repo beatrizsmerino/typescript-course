601462d9b71983dfb194adc6007e528e26f756c8
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-09-26 18:44:21 +0200

    JS. Check the type of values before operating and convert strings to numbers.

--- js-only.js
+++ js-only.js
@@ -3,7 +3,11 @@ const input1 = document.getElementById('num1');
 const input2 = document.getElementById('num2');
 
 function add(num1, num2) {
-	return num1 + num2;
+	if (typeof num1 === 'number' && typeof num2 === 'number') {
+		return num1 + num2;
+	} else {
+		return +num1 + +num2
+	}
 }
 
 button.addEventListener('click', function () {
@@ -16,4 +20,7 @@ button.addEventListener('click', function () {
  * This it is a logical error in our application.
  * In JavaScript when you access the value of an input, it is always a string, no matter if the input type is a number.
  * So when passing two string to this function they get concatenated instead of added mathematically.
+ *
+ * How we are using Vanilla Javascript we need to check the type of the values before operating on them.
+ * To fix this, if they are not numbers, we can use the + operator to convert the string to a number.
  */
