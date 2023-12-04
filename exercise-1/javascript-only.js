69d9db177ae2027af464bc48c64d86dd5911f1f0
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-12-04 11:12:15 +0100

    style(exercises-1-2): format code with `prettier`

--- exercise-1/javascript-only.js
+++ exercise-1/javascript-only.js
@@ -1,20 +1,20 @@
-(function() {
-const button = document.querySelector('button');
-const input1 = document.getElementById('num1');
-const input2 = document.getElementById('num2');
-const outputResult = document.getElementById('result');
+(function () {
+	const button = document.querySelector("button");
+	const input1 = document.getElementById("num1");
+	const input2 = document.getElementById("num2");
+	const outputResult = document.getElementById("result");
 
-function add(num1, num2) {
-	if (typeof num1 === 'number' && typeof num2 === 'number') {
-		return num1 + num2;
-	} else {
-		return +num1 + +num2
+	function add(num1, num2) {
+		if (typeof num1 === "number" && typeof num2 === "number") {
+			return num1 + num2;
+		} else {
+			return +num1 + +num2;
+		}
 	}
-}
 
-button.addEventListener('click', function () {
-	const result = add(input1.value, input2.value);
-	outputResult.innerHTML = result;
-	console.log(result);
-});
+	button.addEventListener("click", function () {
+		const result = add(input1.value, input2.value);
+		outputResult.innerHTML = result;
+		console.log(result);
+	});
 })();
--- exercise-2/typescript-only.ts
+++ exercise-2/typescript-only.ts
@@ -1,14 +1,14 @@
-(function() {
-const button = document.querySelector('button');
-const input1 = document.getElementById('num1')! as HTMLInputElement;
-const input2 = document.getElementById('num2')! as HTMLInputElement;
+(function () {
+	const button = document.querySelector("button");
+	const input1 = document.getElementById("num1")! as HTMLInputElement;
+	const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-function add(num1: number, num2: number) {
-	return num1 + num2;
-}
+	function add(num1: number, num2: number) {
+		return num1 + num2;
+	}
 
-button.addEventListener('click', function () {
-	const result = add(+input1.value, +input2.value);
-	console.log(result);
-});
+	button.addEventListener("click", function () {
+		const result = add(+input1.value, +input2.value);
+		console.log(result);
+	});
 })();
