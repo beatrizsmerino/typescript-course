5d46275754b1f69e0b9b154c937efbd4d1842177
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-12-04 10:46:13 +0100

    feat(exercise-1): add the result of the sum in the DOM

--- exercise-1/javascript-only.js
+++ exercise-1/javascript-only.js
@@ -1,6 +1,7 @@
 const button = document.querySelector('button');
 const input1 = document.getElementById('num1');
 const input2 = document.getElementById('num2');
+const outputResult = document.getElementById('result');
 
 function add(num1, num2) {
 	if (typeof num1 === 'number' && typeof num2 === 'number') {
@@ -12,5 +13,6 @@ function add(num1, num2) {
 
 button.addEventListener('click', function () {
 	const result = add(input1.value, input2.value);
+	outputResult.innerHTML = result;
 	console.log(result);
 });
