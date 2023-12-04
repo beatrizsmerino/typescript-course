43e8c0916e2e1e001e6b0f5b4829b34a84948693
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-12-04 11:58:53 +0100

    refactor(exercise-1): shows that `input` data always comes in `string` form

--- exercise-1/javascript-only.js
+++ exercise-1/javascript-only.js
@@ -5,11 +5,7 @@
 	const outputResult = document.getElementById("result");
 
 	function add(num1, num2) {
-		if (typeof num1 === "number" && typeof num2 === "number") {
-			return num1 + num2;
-		} else {
-			return +num1 + +num2;
-		}
+		return num1 + num2;
 	}
 
 	button.addEventListener("click", function () {
