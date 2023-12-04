a2323f4548e2a2143dc72d93bcacaea53635b273
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-12-04 20:01:18 +0100

    feat(exercise-3): training `number`, `string` and `boolean` types

--- exercises/exercise-3/scripts.js
+++ exercises/exercise-3/scripts.js
@@ -1,3 +1,26 @@
 "use strict";
 (function () {
+    function add(num1, num2, showResult, phrase) {
+        if (typeof num1 !== "number" || typeof num2 !== "number") {
+            throw new Error("Invalid input!");
+        }
+        var result = num1 + num2;
+        if (showResult) {
+            console.log(phrase + result);
+        }
+        else {
+            return result;
+        }
+    }
+    // const number1 = 5; // 5.0
+    // let number1;
+    // number1 = '5'; // It's allowed
+    var number1; // It's better to indicate the type instead wait for Typescript inference.
+    number1 = 5;
+    var number2 = 2.8;
+    var printResult = true;
+    var resultPhrase = "Result is: ";
+    // resultPhrase = 3; // Throws an error. It is not allowed change the type.
+    add(number1, number2, printResult, resultPhrase);
+    // add(2, "1", false, ""); // Throws an error. The 'add' function expected a 'number' as a second parameter.
 })();
--- exercises/exercise-3/scripts.ts
+++ exercises/exercise-3/scripts.ts
@@ -1,4 +1,36 @@
 (function () {
+	function add(
+		num1: number,
+		num2: number,
+		showResult: boolean,
+		phrase: string
+	) {
+		if (typeof num1 !== "number" || typeof num2 !== "number") {
+			throw new Error("Invalid input!");
+		}
 
+		const result = num1 + num2;
+
+		if (showResult) {
+			console.log(phrase + result);
+		} else {
+			return result;
+		}
+	}
+
+	// const number1 = 5; // 5.0
+
+	// let number1;
+	// number1 = '5'; // It's allowed
+
+	let number1: number; // It's better to indicate the type instead wait for Typescript inference.
+	number1 = 5;
+
+	const number2 = 2.8;
+	const printResult = true;
+	let resultPhrase = "Result is: ";
+	// resultPhrase = 3; // Throws an error. It is not allowed change the type.
+
+	add(number1, number2, printResult, resultPhrase);
+	// add(2, "1", false, ""); // Throws an error. The 'add' function expected a 'number' as a second parameter.
 })();
-
