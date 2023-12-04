0ee8f1ee10bbf289d33454f2f4d646fea5c6b732
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-12-04 13:04:45 +0100

    fix(exercise-2): some DOM elements may not exist
    Error:
    - "button" is possibly "null".ts(18047)
    
    Solution:
    - Check if `button` exist before access it

--- exercise-2/typescript-only.ts
+++ exercise-2/typescript-only.ts
@@ -7,8 +7,10 @@
 		return num1 + num2;
 	}
 
-	button.addEventListener("click", function () {
-		const result = add(+input1.value, +input2.value);
-		console.log(result);
-	});
+	if(button) {
+		button.addEventListener("click", function () {
+			const result = add(+input1.value, +input2.value);
+			console.log(result);
+		});
+	}
 })();
