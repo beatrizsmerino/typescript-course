adcb726831a63793aff09b2a8abeaf284ea19e90
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-12-04 15:28:52 +0100

    build(npm): regenerate the `js`

--- exercises/exercise-2/typescript-only.js
+++ exercises/exercise-2/typescript-only.js
@@ -1,10 +1,15 @@
-var button = document.querySelector('button');
-var input1 = document.getElementById('num1');
-var input2 = document.getElementById('num2');
-function add(num1, num2) {
-    return num1 + num2;
-}
-button.addEventListener('click', function () {
-    var result = add(+input1.value, +input2.value);
-    console.log(result);
-});
+"use strict";
+(function () {
+    var button = document.querySelector("button");
+    var input1 = document.getElementById("num1");
+    var input2 = document.getElementById("num2");
+    function add(num1, num2) {
+        return num1 + num2;
+    }
+    if (button) {
+        button.addEventListener("click", function () {
+            var result = add(+input1.value, +input2.value);
+            console.log(result);
+        });
+    }
+})();
