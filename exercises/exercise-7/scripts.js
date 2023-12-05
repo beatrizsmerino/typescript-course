c7cfd2e7e19e67b119dc7e77cf9624e1530aa3bf
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-12-06 00:13:05 +0100

    feat(exercise-7): training `any`, `unknown` and `never` types

--- exercises/exercise-7/scripts.js
+++ exercises/exercise-7/scripts.js
@@ -1,4 +1,17 @@
 "use strict";
 (function () {
     console.log("Hello from exercise 7!");
+    var userInput;
+    var userName;
+    var user;
+    userInput = 5;
+    userInput = "Hello world!";
+    userName = user;
+    if (typeof userInput === "string") {
+        userName = userInput;
+    }
+    function generateError(message, code) {
+        throw { messageError: message, codeError: code };
+    }
+    generateError("An error occurred", 500);
 })();
--- exercises/exercise-7/scripts.ts
+++ exercises/exercise-7/scripts.ts
@@ -1,3 +1,21 @@
 (function () {
 	console.log("Hello from exercise 7!");
+
+	let userInput: unknown; // Must check the types
+	let userName: string;
+	let user: any; // Disable Typescript errors
+
+	userInput = 5; // All values permitted
+	userInput = "Hello world!";
+	// userName = userInput; // Throws an error
+	userName = user; // The 'any' type is more flexible than 'unknown' type
+
+	if (typeof userInput === "string") {
+		userName = userInput;
+	}
+
+	function generateError(message: string, code: number): never {
+		throw { messageError: message, codeError: code }; // interrupt code execution
+	}
+	generateError("An error occurred", 500);
 })();
