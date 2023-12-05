a09c9cf19bc318a1f20fd440423fbf08348b61b4
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-12-05 23:16:37 +0100

    feat(exercise-6): training `function return` and `callback function` types

--- exercises/exercise-6/scripts.js
+++ exercises/exercise-6/scripts.js
@@ -1,4 +1,21 @@
 "use strict";
 (function () {
     console.log("Hello from exercise 6!");
+    function add(input1, input2) {
+        return input1 + input2;
+    }
+    function printResult(input) {
+        console.log("Result: ".concat(input));
+    }
+    printResult(add(1, 2));
+    var combineValues;
+    combineValues = add;
+    console.log(combineValues(8, 8));
+    function addAndHandle(input1, input2, callbackFunction) {
+        var result = input1 + input2;
+        callbackFunction(result);
+    }
+    addAndHandle(10, 20, function (result) {
+        console.log(result);
+    });
 })();
--- exercises/exercise-6/scripts.ts
+++ exercises/exercise-6/scripts.ts
@@ -1,3 +1,48 @@
 (function () {
 	console.log("Hello from exercise 6!");
+
+	// function add(input1: number, input2: number): string { // Throws an error
+	// 	return input1 + input2;
+	// }
+
+	function add(input1: number, input2: number): number {
+		return input1 + input2;
+	}
+
+	// function printResult(input: number): undefined {
+	// 	console.log(`Result: ${input}`);
+	// 	return;
+	// }
+	function printResult(input: number): void {
+		console.log(`Result: ${input}`);
+	}
+	printResult(add(1, 2));
+
+	// let combineValues;
+	// combineValues = add;
+	// combineValues = 5; // Throws an error
+	// console.log(combineValues(8, 8));
+
+	// let combineValues: Function;
+	// combineValues = add;
+	// combineValues = printResult; // Throws an error
+	// console.log(combineValues(8, 8));
+
+	let combineValues: (a: number, b: number) => number; // Function types
+	combineValues = add;
+	// combineValues = printResult; // Throws an error
+	console.log(combineValues(8, 8));
+
+	function addAndHandle(
+		input1: number,
+		input2: number,
+		callbackFunction: (input: number) => void
+	) {
+		const result = input1 + input2;
+		callbackFunction(result);
+	}
+
+	addAndHandle(10, 20, (result) => {
+		console.log(result);
+	});
 })();
