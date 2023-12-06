9ffc4f078720261062e5779ae7602a1d3c57bd2a
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-12-06 12:02:35 +0100

    feat(exercise-9): using `strictNullChecks: false` rule into `tsconfig`

--- exercises/exercise-9/src/scripts.ts
+++ exercises/exercise-9/src/scripts.ts
@@ -3,8 +3,12 @@
 
 	console.log("This is the 'scripts.js' file");
 
-	const button = document.querySelector("button")!;
-	button.addEventListener("click", () => {
+	// The '!' sign asks Typescript to trust that it will find the element. But this is not a sure thing, for JavaScript if the element does not exist in the HTML it will throw an error.
+	const button1 = document.querySelector("#button1")!;
+	
+	// Using the 'strictNullChecks: false' rule in 'tsconfig' will not throw an error on DOM elements.
+	const button2 = document.querySelector("#button2"); // null
+	button2.addEventListener("click", () => {
 		console.log("Clicked!");
 	});
 })();
