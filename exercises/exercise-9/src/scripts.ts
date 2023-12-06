5eec911c3035c9277da0b98631fdd38e571c7c73
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-12-06 12:24:57 +0100

    style(exercises): format code with `prettier`

--- exercises/exercise-9/src/scripts.ts
+++ exercises/exercise-9/src/scripts.ts
@@ -5,7 +5,7 @@
 
 	// The '!' sign asks Typescript to trust that it will find the element. But this is not a sure thing, for JavaScript if the element does not exist in the HTML it will throw an error.
 	const button1 = document.querySelector("#button1")!;
-	
+
 	// Using the 'strictNullChecks: false' rule in 'tsconfig' will not throw an error on DOM elements.
 	const button2 = document.querySelector("#button2"); // null
 	// button2.addEventListener("click", () => {
@@ -14,7 +14,7 @@
 
 	// It is recommended to use 'strictNullChecks: true' and check if the button exists with Javascript
 	const button3 = document.querySelector("#button3");
-	if(button3) {
+	if (button3) {
 		button3.addEventListener("click", () => {
 			console.log("Clicked!");
 		});
