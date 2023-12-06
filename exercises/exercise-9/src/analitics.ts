9a6188267b7cf96e1269d2df83a6935e9f0c8862
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-12-06 11:06:19 +0100

    feat(exercise-9): using `noImplicitAny` rule into `tsconfig`

--- exercises/exercise-9/src/analitics.ts
+++ exercises/exercise-9/src/analitics.ts
@@ -6,5 +6,13 @@
 	// Uncomment this error to see how the `noEmitOnError` rule in `tsconfig` works.
 	// const name: string = 30; // Throws an error
 
-
+	// Variables are more flexible, allowing the type 'any', unlike function parameters.
+	let logged; // any
+	// Disabling the 'noImplicitAny' rule in 'tsconfig' will not throw an error in the function where 'data' is required to have a type assigned.
+	function sendAnalitics(data) { // any
+		console.log(data); // string
+		logged = true; // boolean
+		logged = "Beatriz"; // string
+	}
+	sendAnalitics("The data");
 })();
