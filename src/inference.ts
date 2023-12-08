29505752cf5aa56447f8dfcf012f45c9f57fd089
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2023-12-08 20:00:30 +0100

    feat(inference): concept of inference

---
+++ src/inference.ts
@@ -0,0 +1,11 @@
+console.log("📄", "inference.ts");
+
+(function () {
+	const a = 1; // 1
+	const b = 2; // 2
+	const c = a + b; // 3
+
+	console.log(a, typeof a); // number
+	console.log(b, typeof b); // number
+	console.log(c, typeof c); // number
+})();
